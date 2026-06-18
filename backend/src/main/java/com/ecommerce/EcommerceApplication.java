21db388cc832516d56032c61fcc91ea3ccd0fe4b
Eswar <dharanigk2018@gmail.com>
2026-06-18 15:35:15 +0530

    Corrected annotation

--- backend/src/main/java/com/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/ecommerce/EcommerceApplication.java
@@ -3,7 +3,7 @@ package com.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationś
+@SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
