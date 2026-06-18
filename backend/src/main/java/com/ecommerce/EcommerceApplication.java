ed50161eb7ab71da8da9fd4c4594c573eda06076
Eswar <dharanigk2018@gmail.com>
2026-06-18 17:12:18 +0530

    Added MockMvc test for controllers

--- backend/src/main/java/com/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,12 +1,23 @@
 package com.ecommerce;
 
+import com.ecommerce.product.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(Environment environment) {
+        return runner -> {
+          System.out.println("Server running on port: " + environment.getProperty("local.server.port"));
+        };
     }
 }
